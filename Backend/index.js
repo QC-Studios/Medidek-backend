b45e65f6f0d45f953a5efce67c6201b8a1fca3d6
sometimes-abhay <brandbuilder97@gmail.com>
2023-08-25 22:34:13 +0530

    origin access for local host

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ const app = express()
 // Use body-parser middleware for parsing JSON and url-encoded request bodies
 app.use(cors({
     credentials:true,
-    origin:'https://www.medidek.in'
+    origin:['https://www.medidek.in', 'http://localhost:5173']
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
